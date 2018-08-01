2ba74b7b7afb6641159ce2e0ce508c27ef820c08
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-01 20:49:32 +0200

    Compilo interfaces

---
+++ Typescript/Interfaces/1.js
@@ -0,0 +1,14 @@
+"use strict";
+/**
+ * @author Raúl Caro Pastorino (fryntiz)
+ * @copyright Copyright © 2018 Raúl Caro Pastorino
+ * @license https://wwww.gnu.org/licenses/gpl.txt
+ *
+ * https://fryntiz.es
+ */
+var camaleon = {
+    tipo: "reptil",
+    peculiaridad: "camuflarse",
+    sizecm: 20
+};
+console.log('CAMALEON', camaleon);
