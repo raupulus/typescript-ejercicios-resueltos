2a02f8a964e9d5718121dc706e6fa7c89942629d
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-01 21:34:07 +0200

    Planteo actividad de herencia de clases

---
+++ Typescript/Clases/2-Figuras-Herencia.ts
@@ -0,0 +1,26 @@
+/**
+ * @author Raúl Caro Pastorino (fryntiz)
+ * @copyright Copyright © 2018 Raúl Caro Pastorino
+ * @license https://wwww.gnu.org/licenses/gpl.txt
+ *
+ * https://fryntiz.es
+ */
+
+/*
+ * ↓ INSTRUCCIONES DEL EJERCICIO ↓
+ *
+ * Se requiere ser lo más específico posible en cuanto a tipos y uso estricto
+ * para typescript definiendo y ajustándose a concretar (usar ES6)
+ */
+
+/*
+ * A partir del ejercicio anterior "1-Rectángulo.ts" deberá sacar 2 clases con
+ * una padre de la cual heredar factores comunes.
+ *
+ * Deberá crear una clase para contemplar un rectángulo y un triángulo con dos * propiedades:
+ * 1 → Base
+ * 2 → Altura
+ *
+ * También deberá tener un método para calcular el área en el rectángulo:
+ *(Base*Altura) y en el triángulo: ((Base*Altura)/2)
+ */
