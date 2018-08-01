19a42ff9589f463d35d0ee08bbbed3c7e4507dbc
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-01 20:54:24 +0200

    Actividad función arrow planteada

---
+++ Typescript/Funciones/Arrow/1.ts
@@ -0,0 +1,26 @@
+/**
+ * @author Raúl Caro Pastorino (fryntiz)
+ * @copyright Copyright © 2018 Raúl Caro Pastorino
+ * @license https://wwww.gnu.org/licenses/gpl.txt
+ *
+ * https://fryntiz.es
+ */
+
+/*
+ * ↓ INSTRUCCIONES DEL EJERCICIO ↓
+ *
+ * Se requiere ser lo más específico posible en cuanto a tipos y uso estricto
+ * para typescript definiendo y ajustándose a concretar (usar ES6)
+ */
+
+/* Convertir las siguientes funciones a funciones flecha */
+
+/*
+function obtenerDoble( a, b ){
+  return (a + b) * 2
+}
+
+function obtenerTriple( a, b, c ){
+  return (a + b +c) * 3
+}
+*/
