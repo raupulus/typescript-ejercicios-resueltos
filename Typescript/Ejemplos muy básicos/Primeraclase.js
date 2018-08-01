9e4518fc126f66823dd3c88da0696e716309ae59
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-01 20:28:34 +0200

    Recompilo ejemplo básico con la primera clase

--- Typescript/Ejemplos muy básicos/Primeraclase.js
+++ Typescript/Ejemplos muy básicos/Primeraclase.js
@@ -1,3 +1,20 @@
+"use strict";
+/**
+ * @author Raúl Caro Pastorino (fryntiz)
+ * @copyright Copyright © 2018 Raúl Caro Pastorino
+ * @license https://wwww.gnu.org/licenses/gpl.txt
+ *
+ * https://fryntiz.es
+ */
+/*
+ * ↓ INSTRUCCIONES DEL EJERCICIO ↓
+ *
+ * Se requiere ser lo más específico posible en cuanto a tipos y uso estricto
+ * para typescript definiendo y ajustándose a concretar (usar ES6)
+ *
+ * Deberá crear una clase con los una variable pública de cada tipo, un
+ * constructor, instanciar un objeto y luego compilar el typescript a js.
+ */
 var Primeraclase = /** @class */ (function () {
     function Primeraclase() {
         this.texto = 'Cadena de texto';
