bb01ee7d75f73363f2e7f5f3dbd2208f64e8f5a6
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-01 20:28:13 +0200

    Compilo actividad de let y const

---
+++ Typescript/Uso de let y const/1.js
@@ -0,0 +1,33 @@
+"use strict";
+/**
+ * @author Raúl Caro Pastorino (fryntiz)
+ * @copyright Copyright © 2018 Raúl Caro Pastorino
+ * @license https://wwww.gnu.org/licenses/gpl.txt
+ *
+ * https://fryntiz.es
+ */
+/*
+ * ↓ INSTRUCCIONES DEL EJERCICIO ↓
+ *
+ * Se requiere ser lo más específico posible en cuanto a tipos y uso estricto
+ * para typescript definiendo y ajustándose a concretar (usar ES6)
+ */
+/* Se partirá del siguiente código */
+/*
+var nombre = "Manolo el del bombo";
+var edad = 23;
+var PERSONAJE = {
+  nombre: nombre,
+  edad: edad
+};
+*/
+var nombre = "Manolo el del bombo";
+var edad = 23;
+var PERSONAJE = {
+    nombre: nombre,
+    edad: edad
+};
+console.log(nombre);
+console.log(edad);
+console.log(PERSONAJE.nombre);
+console.log(PERSONAJE.edad);
