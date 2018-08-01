29b9515d21674173364ce3fc6ac2378467e76c07
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-01 17:53:17 +0200

    Planteo actividad practicando con let y const

---
+++ Typescript/Uso de let y const/1.ts
@@ -0,0 +1,25 @@
+/**
+ * @author Raúl Caro Pastorino (fryntiz)
+ * @copyright Copyright © 2018 Raúl Caro Pastorino
+ * @license https://wwww.gnu.org/licenses/gpl.txt
+ *
+ * https://fryntiz.es
+ */
+
+/*
+ * ↓ INSTRUCCIONES DEL EJERCICIO ↓
+ *
+ * Se requiere ser lo más específico posible en cuanto a tipos y uso estricto
+ * para typescript definiendo y ajustándose a concretar (usar ES6)
+ */
+
+/* Se partirá del siguiente código */
+
+/*
+var nombre = "Manolo el del bombo";
+var edad = 23;
+var PERSONAJE = {
+  nombre: nombre,
+  edad: edad
+};
+*/
