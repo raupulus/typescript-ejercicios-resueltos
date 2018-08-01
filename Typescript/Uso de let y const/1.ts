62a8490852800785b9753ea81840df8231c6adad
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-01 20:28:02 +0200

    Completo actividad sobre let y const

--- Typescript/Uso de let y const/1.ts
+++ Typescript/Uso de let y const/1.ts
@@ -23,3 +23,17 @@ var PERSONAJE = {
   edad: edad
 };
 */
+
+let nombre:string = "Manolo el del bombo";
+
+let edad:number = 23;
+
+const PERSONAJE : { nombre:string, edad:number} = {
+  nombre: nombre,
+  edad: edad
+};
+
+console.log(nombre);
+console.log(edad);
+console.log(PERSONAJE.nombre);
+console.log(PERSONAJE.edad);
