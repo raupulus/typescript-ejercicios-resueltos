83064c5fa5132c2428a46740706c39efc23bac1f
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-01 20:59:09 +0200

    Actividad de función arrow completada

--- Typescript/Funciones/Arrow/1.ts
+++ Typescript/Funciones/Arrow/1.ts
@@ -24,3 +24,9 @@ function obtenerTriple( a, b, c ){
   return (a + b +c) * 3
 }
 */
+
+let obtenerDoble = ( a:number, b:number ) => (a + b) * 2;
+console.log('OBTENERDOBLE', obtenerDoble(2,3));  // 5*2 = 10
+
+let obtenerTriple = ( a:number, b:number, c:number ) => (a + b +c) * 3;
+console.log('OBTENERTRIPLE', obtenerTriple(2,3,5));  // 10*3 = 30
