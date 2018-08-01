abb144a2d3425050e7ee0812a8a081a018b2c7ac
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-01 20:44:36 +0200

    Objetivos de la actividad planteada

---
+++ Typescript/Interfaces/1.ts
@@ -0,0 +1,26 @@
+/**
+ * @author Raúl Caro Pastorino (fryntiz)
+ * @copyright Copyright © 2018 Raúl Caro Pastorino
+ * @license https://wwww.gnu.org/licenses/gpl.txt
+ *
+ * https://fryntiz.es
+ */
+
+/*
+ * ↓ INSTRUCCIONES DEL EJERCICIO ↓
+ *
+ * Se requiere ser lo más específico posible en cuanto a tipos y uso estricto
+ * para typescript definiendo y ajustándose a concretar (usar ES6)
+ */
+
+/*
+ * Dado el código siguiente, tendremos que crear una interfaz que sirva
+ * después para validar el objeto.
+ */
+
+/*
+var camaleon = {
+  tipo: "reptil",
+  peculiaridad: "camuflarse"
+}
+*/
