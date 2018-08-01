1b2965e28e168ad69b72d560dbb8abbb23e6d370
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-01 21:08:18 +0200

    Se plantea la actividad para una función con distintos tipos de parámetros

---
+++ Typescript/Funciones/1 - Parámetros Opcionales y Obligatorios.ts
@@ -0,0 +1,35 @@
+/**
+ * @author Raúl Caro Pastorino (fryntiz)
+ * @copyright Copyright © 2018 Raúl Caro Pastorino
+ * @license https://wwww.gnu.org/licenses/gpl.txt
+ *
+ * https://fryntiz.es
+ */
+
+/*
+ * ↓ INSTRUCCIONES DEL EJERCICIO ↓
+ *
+ * Se requiere ser lo más específico posible en cuanto a tipos y uso estricto
+ * para typescript definiendo y ajustándose a concretar (usar ES6)
+ */
+
+/*
+ * La siguiente función contendrá parámetros obligatorios, opcionales y otros
+ * serán por defecto si no le pasamos ninguno.
+ *
+ * Deberás adaptarlos de la siguiente forma:
+ * nombre → obligatorio
+ * edad → opcional
+ * arma → por defecto u omisión será "pistola"
+ */
+
+/*
+function getVillano( nombre, edad, arma ) {
+  var mensaje;
+  if( edad ) {
+    mensaje = nombre + " tiene una edad de: " + edad + " y arma: " + arma;
+  } else {
+    mensaje = nombre + " tiene un " + edad
+  }
+};
+*/
