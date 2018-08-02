64475c5733ddfc3eac994afc1fa00b2071a60943
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-02 10:11:31 +0200

    Compilo actividad de clase rectángulo

---
+++ Typescript/Clases/1-Rectangulo.js
@@ -0,0 +1,34 @@
+"use strict";
+/**
+ * @author Raúl Caro Pastorino (fryntiz)
+ * @copyright Copyright © 2018 Raúl Caro Pastorino
+ * @license https://wwww.gnu.org/licenses/gpl.txt
+ *
+ * https://fryntiz.es
+ */
+/*
+ * ↓ INSTRUCCIONES DEL EJERCICIO ↓
+ *
+ * Se requiere ser lo más específico posible en cuanto a tipos y uso estricto
+ * para typescript definiendo y ajustándose a concretar (usar ES6)
+ */
+/*
+ * Deberá crear una clase para contemplar un rectángulo con dos propiedades:
+ * 1 → Base
+ * 2 → Altura
+ *
+ * También deberá tener un método para calcular el área (Base*Altura), este
+ * método deberá devolver un valor numérico.
+ */
+var Rectangulo = /** @class */ (function () {
+    function Rectangulo(base, altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+    Rectangulo.prototype.calcularArea = function () {
+        return this.base * this.altura;
+    };
+    return Rectangulo;
+}());
+console.log('RECTANGULO-1', new Rectangulo(2, 6).calcularArea());
+console.log('RECTANGULO-2', new Rectangulo(3, 9).calcularArea());
