70b1601b21fefe30cd0b4f0fa9dfa0e75027f9ef
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-02 10:33:13 +0200

    Actividad de clases con herencia resuelta

--- Typescript/Clases/2-Figuras-Herencia.ts
+++ Typescript/Clases/2-Figuras-Herencia.ts
@@ -24,3 +24,52 @@
  * También deberá tener un método para calcular el área en el rectángulo:
  *(Base*Altura) y en el triángulo: ((Base*Altura)/2)
  */
+
+/**
+ * Clase Figura que será una clase padre de la que hereda cada figura
+ * @param base   number Valor de la base
+ * @param altura number Valor de la altura
+ */
+class Figura {
+    public base:number;
+    public altura:number;
+
+    constructor(base:number, altura:number) {
+        this.base = base;
+        this.altura = altura
+    }
+}
+
+/**
+ * Clase Rectángulo que hereda de Figura
+ * @param base   number Valor de la base
+ * @param altura number Valor de la altura
+ */
+class Rect extends Figura {
+    constructor(base:number, altura:number) {
+        super(base, altura);
+    }
+
+    public calcularArea() {
+        return this.base * this.altura;
+    }
+}
+console.log('RECTANGULO-1', new Rect(2,6).calcularArea());
+console.log('RECTANGULO-2', new Rect(3,9).calcularArea());
+
+/**
+ * Clase Triángulo que hereda de Figura
+ * @param base   number Valor de la base
+ * @param altura number Valor de la altura
+ */
+class Triangulo extends Figura {
+    constructor(base:number, altura:number) {
+        super(base, altura);
+    }
+
+    public calcularArea() {
+        return (this.base * this.altura)/2;
+    }
+}
+console.log('TRIANGULO-1', new Triangulo(2,6).calcularArea());
+console.log('TRIANGULO-2', new Triangulo(3,9).calcularArea());
