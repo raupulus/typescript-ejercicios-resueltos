0dbdcb7d83c6ccd01b3cdd174f33edec3e2e9559
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-02 10:11:18 +0200

    Completo actividad de clase Rectangulo

--- Typescript/Clases/1-Rectangulo.ts
+++ Typescript/Clases/1-Rectangulo.ts
@@ -21,3 +21,18 @@
  * También deberá tener un método para calcular el área (Base*Altura), este
  * método deberá devolver un valor numérico.
  */
+class Rectangulo {
+    private base:number;
+    private altura:number;
+
+    constructor(base:number, altura:number) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public calcularArea() {
+        return this.base * this.altura;
+    }
+}
+console.log('RECTANGULO-1', new Rectangulo(2,6).calcularArea());
+console.log('RECTANGULO-2', new Rectangulo(3,9).calcularArea());
